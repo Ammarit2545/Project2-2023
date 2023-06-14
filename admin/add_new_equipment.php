f1328eeef24979cfa5facf76ddb89a373bba6ae0
chutima <chutimamaa2544@gmail.com>
2023-06-14 15:43:16 +0700

    2566-06-14 - Test

---
+++ admin/add_new_equipment.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include('../database/condb.php');
+
+if (!isset($_SESSION['role_id'])) {
+    header('Location:../home.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page - Create Auto Create Serial Number</title>
+</head>
+<body>
+    
+</body>
+</html>
