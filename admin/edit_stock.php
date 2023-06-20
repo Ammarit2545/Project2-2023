9827bd5e2c71e62b776efd5afdc35e31251e42a6
Arm <armm4sonic@gmail.com>
2023-06-21 02:26:13 +0700

    2566-06-20 - Add Stock

--- admin/edit_stock.php
+++ admin/edit_stock.php
@@ -185,7 +185,7 @@ if (!isset($_SESSION['role_id'])) {
 
                                 <!-- Page Heading -->
                                 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                                    <h1 class="h3 mb-0 text-gray-800">เพิ่มอะไหล่ในสต๊อก</h1>
+                                    <h1 class="h3 mb-0 text-gray-800">จัดการอะไหล่</h1>
                                 </div>
 
                                 <?php
