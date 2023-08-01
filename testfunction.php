ca9e68b421d1674720eef5ed2467c46bba51eedf
Arm <armm4sonic@gmail.com>
2023-08-02 02:14:43 +0700

    2566-08-02 - Make New Line Login

--- testfunction.php
+++ testfunction.php
@@ -1,26 +1,3 @@
 <?php
-if (!isset($_SESSION['profile'])) {
-    header("location:index.php");
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
-    if (isset($_SESSION['profile'])) {
-        $profile = $_SESSION['profile'];
-    }
-    ?>
-    <h1>Welcome , <?= $profile->name; ?></h1>
-    <p class="lead">Your Email : <?= $profile->email; ?></p>
-    <img src="<?php echo $profile->picture  ?>" class="rounded" alt="Profile Picture">
-</body>
-
-</html>
+// Replace 'Project2-2023' with the folder name you want to select
+echo $_SERVER['HTTP_HOST'];
