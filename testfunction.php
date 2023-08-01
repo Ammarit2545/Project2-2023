de45bdfdd7eba13bbb2b416988be64db16e15afa
Arm <armm4sonic@gmail.com>
2023-08-02 02:16:40 +0700

    2566-08-02 - Make New Line Login

--- testfunction.php
+++ testfunction.php
@@ -1,3 +1,17 @@
 <?php
 // Replace 'Project2-2023' with the folder name you want to select
-echo $_SERVER['HTTP_HOST'];
+$selectedFolder = '../';
+
+// Get the path of the selected folder
+$selectedFolderPath = __DIR__ . '/' . $selectedFolder;
+
+// Use dirname twice to get the mother folder path
+$motherFolderPath = dirname(dirname($selectedFolderPath));
+
+// Use basename to get the name of the mother folder
+$motherFolderName = basename($motherFolderPath);
+
+// Display the name of the mother folder
+echo "The mother folder is: " . $motherFolderName . "<br> ";
+
+?>
