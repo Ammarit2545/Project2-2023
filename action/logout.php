f8ea9620b21fdc48a02cfb8dff569d67b59c5b4f
Arm <armm4sonic@gmail.com>
2023-08-01 17:44:28 +0700

    2566-08-01 - Complete Line Login And Insert To DB And Log Member

--- action/logout.php
+++ action/logout.php
@@ -1,34 +1,39 @@
 <?php
-    session_start();
-    include('../database/condb.php');
-    if(!isset($_SESSION['role_id'])){
-        $log_id = $_SESSION['log_id'];
-        echo $log_id;
-    
-        $sql = "UPDATE `log_member` SET `date_out`= NOW() WHERE m_log_id = '$log_id'";
-        if (mysqli_query($conn, $sql)) {
+session_start();
+include('../database/condb.php');
+require_once('line_login.php');
+if (!isset($_SESSION['role_id'])) {
+    $log_id = $_SESSION['log_id'];
+    echo $log_id;
+
+    $sql = "UPDATE `log_member` SET `date_out`= NOW() WHERE m_log_id = '$log_id'";
+    if (mysqli_query($conn, $sql)) {
         echo "Record updated successfully";
-        } else {
+    } else {
         echo "Error updating record: " . mysqli_error($conn);
-        }
-    
-        session_destroy();
-        echo "<script> alert(' ออกจากระบบเสร็จสิ้นแล้ว '); </script>";
-        header("location:../home.php");
-    }elseif(isset($_SESSION['role_id'])){
-        $log_id = $_SESSION['log_id'];
-        echo $log_id;
-    
-        $sql = "UPDATE `log_employee` SET `date_out`= NOW() WHERE e_log_id = '$log_id'";
-        if (mysqli_query($conn, $sql)) {
-        echo "Record updated successfully";
-        } else {
-        echo "Error updating record: " . mysqli_error($conn);
-        }
-    
-        session_destroy();
-        echo "<script> alert(' ออกจากระบบเสร็จสิ้นแล้ว '); </script>";
-        header("location:../home.php");
     }
 
-?>
+    session_destroy();
+    echo "<script> alert(' ออกจากระบบเสร็จสิ้นแล้ว '); </script>";
+    header("location:../index.php");
+} elseif (isset($_SESSION['role_id'])) {
+    $log_id = $_SESSION['log_id'];
+    echo $log_id;
+
+    $sql = "UPDATE `log_employee` SET `date_out`= NOW() WHERE e_log_id = '$log_id'";
+    if (mysqli_query($conn, $sql)) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . mysqli_error($conn);
+    }
+
+    session_destroy();
+    echo "<script> alert(' ออกจากระบบเสร็จสิ้นแล้ว '); </script>";
+    header("location:../index.php");
+} elseif (isset($_SESSION['profile'])) {
+    $profile = $_SESSION['profile'];
+    $line = new LineLogin();
+    $line->revoke($profile->access_token);
+    session_destroy();
+    header("location:../index.php");
+}
---
+++ callback.php
@@ -0,0 +1,135 @@
+<?php
+session_start();
+include('database/condb.php');
+include('action/line_login.php');
+
+$line = new LineLogin();
+$get = $_GET;
+
+$code = $get['code'];
+$state = $get['state'];
+$token = $line->token($code, $state);
+
+if (property_exists($token, 'error')) {
+    $_SESSION['add_data_alert'] = 1;
+    header('location: index.php');
+    exit; // Exit the script after redirect
+}
+
+if ($token->id_token) {
+    $profile_line = $line->profileFormIdToken($token);
+    $_SESSION['profile'] = $profile_line;
+
+    $profile = $profile_line;
+
+    // information Account 
+    $name = $profile->name;
+    $email = $profile->email;
+    $picture = $profile->picture;
+    $line_id = $profile->line_id;
+
+    // Line Account Check Data in DB
+
+    // Use prepared statements to prevent SQL injection
+    $sql = "SELECT * FROM member WHERE m_email = ? AND del_flg = 0;";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $email);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if ($result) {
+        $row_count = mysqli_num_rows($result);
+
+        if ($row_count > 0) {
+            // Matching record found, do something here if needed
+            // You can access the data using $row = mysqli_fetch_array($result);
+            $row = mysqli_fetch_array($result);
+
+            // Store user information in the session
+            $_SESSION["email"] = $row['m_email'];
+            $_SESSION["id"] = $row['m_id'];
+            $_SESSION["tel"] = $row['m_tel'];
+            $_SESSION["fname"] = $row['m_fname'];
+            $_SESSION["lname"] = $row['m_lname'];
+            $_SESSION['Error'] = "";
+            $_SESSION["address"] = $row['m_add'];
+
+            $_SESSION["log_login"] = 0;
+            $m_id = $_SESSION["id"];
+            // Assuming you have already established a database connection in the variable $conn.
+
+            // // Check if the required session variables exist and have valid values.
+            // if (!isset($_SESSION["m_line_id"]) || !isset($_SESSION["m_id"]) || !is_numeric($_SESSION["m_id"])) {
+            //     // Redirect to an error page or handle the error appropriately.
+            //     header('location: error.php');
+            //     exit;
+            // }
+
+            if ($row['m_line_id'] == NULL) {
+                $sql = "UPDATE member SET m_line_id = '$line_id' WHERE m_id = '$m_id'";
+                $result = mysqli_query($conn, $sql);
+                if ($result) {
+                    $_SESSION['add_new_line_alert'] = 0;
+                }
+            }
+
+            $m_id = $row['m_id'];
+
+            $sql = "INSERT INTO `log_member`(`m_id`, `date_in`) VALUES (?, NOW())";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, "i", $m_id);
+            $result = mysqli_stmt_execute($stmt);
+
+            if ($result) {
+                $_SESSION['add_data_alert'] = 0;
+                $_SESSION['add_line_alert'] = 0;
+                header('location: index.php');
+                exit; // Exit the script after redirect
+            }
+        } else {
+            // No matching record found, do something else here
+            $sql = "INSERT INTO member_line (`ml_line_id`, `ml_name`, `ml_date_in`, `ml_email`) VALUES (?, ?, NOW(),?)";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, "sss", $line_id, $name, $email);
+            $result = mysqli_stmt_execute($stmt);
+
+            if ($result) {
+                $sql = "SELECT * FROM member_line WHERE ml_line_id = ? AND del_flg = 0;";
+                $stmt = mysqli_prepare($conn, $sql);
+                mysqli_stmt_bind_param($stmt, "s", $line_id);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+                $row = mysqli_fetch_array($result);
+
+                // Store user information in the session
+                $_SESSION["email"] = $row['ml_email'];
+                $_SESSION["id"] = $row['ml_id'];
+                $_SESSION["tel"] = $row['ml_tel'];
+                $_SESSION["fname"] = $row['ml_fname'];
+                $_SESSION["lname"] = $row['ml_lname'];
+                $_SESSION['Error'] = "";
+                $_SESSION["address"] = $row['ml_add'];
+                $_SESSION["log_login"] = 0;
+
+                $ml_id = $row['ml_id'];
+
+                $sql = "INSERT INTO `log_member`(`ml_id`, `date_in`) VALUES (?, NOW())";
+                $stmt = mysqli_prepare($conn, $sql);
+                mysqli_stmt_bind_param($stmt, "i", $ml_id);
+                $result = mysqli_stmt_execute($stmt);
+
+                if ($result) {
+                    $_SESSION['add_data_alert'] = 0;
+                    header('location: index.php');
+                    exit; // Exit the script after redirect
+                }
+            }
+        }
+    } else {
+        // Error occurred during the query, handle the error appropriately
+        // For example: echo mysqli_error($conn);
+        $_SESSION['add_data_alert'] = 1;
+        header('location: index.php');
+        exit; // Exit the script after redirect
+    }
+}
