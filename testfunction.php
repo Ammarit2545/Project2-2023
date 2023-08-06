93b435eaa9c5cbc949636afb0454fc0d388cff9f
Arm <armm4sonic@gmail.com>
2023-08-06 22:57:13 +0700

    2566-08-06 - Arm testfunction

--- testfunction.php
+++ testfunction.php
@@ -2,10 +2,10 @@
 session_start();
 
 // Assuming you have the values of 'id_repair_ever_1', 'id_repair_ever_2', 'id_repair_ever_3', and 'id_repair_ever_4' stored somewhere, for example:
-// $_SESSION['id_repair_ever_1'] = 123;
-// $_SESSION['id_repair_ever_2'] = 456;
-// $_SESSION['id_repair_ever_3'] = 789;
-// $_SESSION['id_repair_ever_4'] = 101112;
+$_SESSION['id_repair_ever_1'] = 123;
+$_SESSION['id_repair_ever_2'] = 456;
+$_SESSION['id_repair_ever_3'] = 789;
+$_SESSION['id_repair_ever_4'] = 101112;
 
 $i = 1;
 while (isset($_SESSION['id_repair_ever_' . $i])) {
